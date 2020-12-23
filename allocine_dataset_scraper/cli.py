326f2846f8062883d051e8d45d0defc41eec9a7f
Olivier Maillot <maillot.olivier@gmail.com>
2020-12-23 23:55:44 +0100

    :sparkles:Add cli feature with click

--- allocine_dataset_scraper/cli.py
+++ allocine_dataset_scraper/cli.py
@@ -1,11 +1,25 @@
-from allocine_dataset_scraper import AlloCineScraper
+import click
 
-if __name__ == "__main__":
+from allocine_dataset_scraper.allocine_dataset_scraper import AllocineScraper
 
-    scraper = AlloCineScraper(
-        number_of_pages=int(os.getenv("NUM_PAGES")),
-        output_csv_name=os.getenv("DATASET_NAME"),
-        human_pause=int(os.getenv("TIMEOUT")),
+
+@click.command()
+@click.option("--number_of_pages", default=10, help="Number of page to scrape.")
+@click.option("--from_page", default=1, help="First page to scrape.")
+@click.option(
+    "--output_csv_name", default="allocine_movies.csv", help="Output file name."
+)
+@click.option("--pause_scraping", default=[2, 10], help="Range to randomize pause.")
+def exec_cli(
+    number_of_pages: int, from_page: int, output_csv_name: str, pause_scraping: list
+):
+    """Simple program that greets NAME for a total of COUNT times."""
+    scraper = AllocineScraper(
+        number_of_pages, from_page, output_csv_name, pause_scraping
     )
 
-    scraper.start_scraping_movies()
+    scraper.scraping_movies()
+
+
+if __name__ == "__main__":
+    exec_cli()
