2fdd7cacfc2b543ddce11452d254f878b7b1dbdf
Olivier Maillot <maillot.olivier@gmail.com>
2021-09-26 22:35:43 +0200

    :rotating_light::green_heart: Fix black warning and CI

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import find_namespace_packages, setup
 
 setup(
     name="allocine_dataset_scraper",
-    version="2.1",
+    version="2.2.0",
     description="Scraper for AlloCine",
     author="Olivier Maillot",
     author_email="contact@olivier-maillot.fr",
     url="https://github.com/ibmw/allocine-dataset-scraper",
     install_requires=Path("requirements.txt").read_text().splitlines(),  # dependencies
-    packages=find_namespace_packages(include=['allocine_dataset_scraper.*']),
+    packages=find_namespace_packages(include=["allocine_dataset_scraper.*"]),
     include_package_data=False,
     extras_require={"dev": Path("requirements-dev.txt").read_text().splitlines()},
     entry_points={
