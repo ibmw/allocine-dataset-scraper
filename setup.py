eff59029a367e6b422ea6e98a702cdf230e0969e
Olivier Maillot <maillot.olivier@gmail.com>
2021-09-26 22:03:19 +0200

    :arrow_up: Update dependencies

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from pathlib import Path
 
-from setuptools import find_packages, setup
+from setuptools import find_namespace_packages, setup
 
 setup(
     name="allocine_dataset_scraper",
-    version="1.0",
+    version="2.1",
     description="Scraper for AlloCine",
     author="Olivier Maillot",
     author_email="contact@olivier-maillot.fr",
     url="https://github.com/ibmw/allocine-dataset-scraper",
     install_requires=Path("requirements.txt").read_text().splitlines(),  # dependencies
-    packages=find_packages(),
+    packages=find_namespace_packages(include=['allocine_dataset_scraper.*']),
     include_package_data=False,
     extras_require={"dev": Path("requirements-dev.txt").read_text().splitlines()},
+    entry_points={
+        "console_scripts": ["fetch-allocine=allocine_dataset_scraper.run:main"],
+    },
 )
